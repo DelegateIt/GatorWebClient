61db8064dfc72985c474f0456615ccab6920bb05
nathanchapman <nathanchapman@utexas.edu>
2015-11-18 01:07:08 -0600

    Change left-nav to web component

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,7 +59,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   });
 
   // Close drawer after menu item is selected if drawerPanel is narrow
-  app.onDataRouteClick = function() {
+  app.closeDrawer = function() {
     var drawerPanel = document.querySelector('#paperDrawerPanel');
     if (drawerPanel.narrow) {
       drawerPanel.closeDrawer();
