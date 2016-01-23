b5c01fb257ef2510a04b3ebbd3b5bb0e9335105b
Austin Middleton <xuset1@gmail.com>
2016-01-23 17:33:41 -0600

    Added aws test config to web client

---
+++ configs/test.js
@@ -0,0 +1,8 @@
+var GAT = GAT || {};
+
+GAT.config = {
+    "apiUrl": "http://test-gatorapi.elasticbeanstalk.com:80",
+    "notifierUrl": "http://test-gatorapi.elasticbeanstalk.com:8060",
+    "facebookAppId": "922314864520407",
+    "logLevel": "info"
+};
