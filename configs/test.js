c6747251d6b8d403fb531780df50c1d0943de573
Austin Middleton <xuset1@gmail.com>
2016-02-07 16:56:05 -0600

    Updated aws test server hostanme in config

--- configs/test.js
+++ configs/test.js
@@ -1,8 +1,8 @@
 var GAT = GAT || {};
 
 GAT.config = {
-    "apiUrl": "http://test-gatorapi.elasticbeanstalk.com:80",
-    "notifierUrl": "http://test-gatorapi.elasticbeanstalk.com:8060",
+    "apiUrl": "http://test-gatorapi.us-west-2.elasticbeanstalk.com",
+    "notifierUrl": "http://test-gatorapi.us-west-2.elasticbeanstalk.com:8060",
     "facebookAppId": "922314864520407",
     "logLevel": "info"
 };
