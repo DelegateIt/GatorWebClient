ada113be5e9e9a700ec88786d958487aad5b451f
Austin Middleton <xuset1@gmail.com>
2016-02-24 22:51:57 -0600

    Using https for api/notify communication
    track DelegateIt/OrderApi#98

--- configs/production.js
+++ configs/production.js
@@ -1,8 +1,8 @@
 var GAT = GAT || {};
 
 GAT.config = {
-    "apiUrl": "http://api.godelegateit.com",
-    "notifierUrl": "http://notify.godelegateit.com",
+    "apiUrl": "https://api.godelegateit.com",
+    "notifierUrl": "https://notify.godelegateit.com",
     "facebookAppId": "922040531214507",
     "mode": "production",
     "logLevel": "info"
